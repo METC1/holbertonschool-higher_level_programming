b24cc96b2ca5775ef131c77ffb6cc9bde9b810bd
METC <metc_ck1@hotmail.com>
2022-05-17 22:55:58 -0700

    again

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""
+'''
 Define a class Square
-"""
+'''
 
 
 class Square:
-    """
+    '''
     Square attributes
-    """
+    '''
 
     def __init_(self, size):
-        """
+        '''
         Initializes square
 
         size attribute side of square
-        """
+        '''
 
         self.__size = size
