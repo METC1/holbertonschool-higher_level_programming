924b6067d0fbe62c0a688c7f5a43683656fcdb4d
METC <metc_ck1@hotmail.com>
2022-05-18 10:58:52 -0700

    0-safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list, x):
+    i = 0
     for i in range(0, x):
         try:
             print(my_list[i], end="")
-        except StandardError:
+        except IndexError:
             i = i - 1
             break
     print("")
