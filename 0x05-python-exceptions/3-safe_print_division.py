4c327e5356bea77376642a66dd4cc93d95b1a493
METC <metc_ck1@hotmail.com>
2022-05-18 20:31:04 -0700

    3-safe division fixes

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,15 +2,14 @@
 def safe_print_division(a, b):
     i = 0
     res = 0.0
-        try:
-            res = a / b
-            i = 1
-        except (ValueError, TypeError):
-            pass
-        finally:
-            if i == 1:
-                print("Inside result: {}".format(res))
-                return res
-            else:
-                print("Inside result: {}".format("None"))
-            
+    try:
+        res = a / b
+        i = 1
+    except (ValueError, TypeError, ZeroDivisionError):
+        pass
+    finally:
+        if i == 1:
+            print("Inside result: {}".format(res))
+            return res
+        else:
+            print("Inside result: {}".format("None"))
