1aaff540793da7576bc06c7951fd5ad7a90006ec
METC <metc_ck1@hotmail.com>
2022-05-19 17:18:04 -0700

    1-square pycode

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,19 +2,14 @@
 """
 Class Square defines a square
 """
-
-
 class Square:
     """
     Square class
     """
-
-
     def __init__(self, size):
         """
         Initializes square.
         
-
         Size size attribute of the square
         """
 
