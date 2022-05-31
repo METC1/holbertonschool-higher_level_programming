b2cd2e72870b11da947796f9c0d362f7701ebb1e
METC <metc_ck1@hotmail.com>
2022-05-30 22:46:51 -0700

    7-base geometry integer check

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+BaseGeometry with exception
+raises exception on method not implemented
+"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry area implementing
+    """
+
+    def area(self):
+        """
+        Raises exception because method is not implemented
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public method validates if number is an integer
+        """
+        if type(value) != int:
+            raise Typerror("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(nmae))
