be1d90ac3162c7f240a8cd1ab1ee6b8c5beeecbb
METC <metc_ck1@hotmail.com>
2022-05-30 22:17:16 -0700

    6 base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+BaseGeometry with exception
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry area implementing
+    """
+    def area(self:
+            """
+            Raises exception because not implemented
+            """
+
+            raise Exception("area() is not implemented")
