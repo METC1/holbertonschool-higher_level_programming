bf01c043e992592c92995ce10bd5fd48f3988fff
METC <metc_ck1@hotmail.com>
2022-05-30 20:58:39 -0700

    0-lookup downgrade

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,4 @@ Searches attributes and methods of an object
 def lookup(obj):
     """
     Function that returns list of available attributes and methods of an object"""
-    return [method_name for method_name in dir(obj)]
+    return dir(obj)
