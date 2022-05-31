138dbe7ac84e2fedb1279d2203de5804ff0073e3
METC <metc_ck1@hotmail.com>
2022-05-30 21:52:19 -0700

    0-lookup pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,7 +3,12 @@
 Searches attributes and methods of an object
 """
 
+
 def lookup(obj):
     """
-    Function that returns list of available attributes and methods of an object"""
+    Function that returns list of available 
+    attributes and methods of an object
+    """
+
+
     return dir(obj)
