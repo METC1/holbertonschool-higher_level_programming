9663ba864477b1337b4abe125bde5146ef6e8eab
METC <metc_ck1@hotmail.com>
2022-05-30 20:42:49 -0700

    2-is same class pycodestyle fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Function, returns true if the object is an instance of a class specified
+Function, returns true if the object is an
+instance of a class specified
 false if otherwise
 """
 
 
 def is_same_class(obj, a_class):
     """
-    method, returns true if the object specified is exactly an instance of the specified class. otherwise return false.
+    method, returns true if the object specified
+    is exactly an instance of the specified class.
+    otherwise return false.
     """
 
     if isinstance(obj, a_class):
