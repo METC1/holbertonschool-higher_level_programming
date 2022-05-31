cf762f9f7cd1809415884e913453da2ae432c7b8
METC <metc_ck1@hotmail.com>
2022-05-30 22:08:24 -0700

    4 inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Checks if object is an instance of the class provided
+or inherited directly or indirectly
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    method, checks if obj is an instance of a_class that
+    inherited from a_class, directly or indirectly, 
+    true if it is, false  otherwise
+    """
+
+    return issubclass(obj, a_class) and type(obj) != a_class
