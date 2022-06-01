d302fbb8199265ec6f9ed87cc5b79bae99230b86
METC <metc_ck1@hotmail.com>
2022-06-01 16:22:09 -0700

    7-add_item close rror fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,8 +14,8 @@ def my_list():
     reads existing json file and loads list,
     adds arguments to python list and saves to file
     """
-    obj_list = load_from_json_file(filename)
     filename = "add_item.json"
+    obj_list = load_from_json_file(filename)
 
     for i in range(1, len(sys.argv)):
         obj_list.append(str(sys.argv[i]))
@@ -27,13 +27,13 @@ filename = "add_item.json"
 if len(sys.argv) < 2:
     try:
         f = open(filename)
-        filename.close()
+        f.close()
     except IOError:
         save_to_json_file([], filename)
 else:
     try:
         f = open(filename)
-        filename.close()
+        f.close()
         my_list()
     except IOError:
         save_to_json_file([], filename)
