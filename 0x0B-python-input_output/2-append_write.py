552e1eec2b8986a05207814bd3c1d27c6573ae2b
METC <metc_ck1@hotmail.com>
2022-05-31 21:11:21 -0700

    2-append write

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Appends a string at the end of a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    append_write Appends a string at the end of a text file
+    encoded in UTF-8 and returns the number of characters
+    added
+    """
+    with open(filename, mode='a', encoding'utf-8') as f:
+        return f.write(text)
