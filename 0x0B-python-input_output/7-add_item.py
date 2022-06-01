c320e15408537c78c6b194e2f9940bb08406596b
METC <metc_ck1@hotmail.com>
2022-06-01 15:54:49 -0700

    7-add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list
+and then save them to a file
+"""
+import sys
+import json
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+filename = "add_item.json"
+
+
+def my_list():
+    """
+    reads existing json file and loads list,
+    adds arguments to python list and saves to file
+    """
+    obj_list = load_from_json_file(filename)
+
+    for i in range(1, len(sys.argv)):
+        obj_list.append(str(sys.argv[i]))
+
+    save_to_json_file(obj_list, filename)
+
+
+if len(sys.argv) < 2:
+    try:
+        f = open(filename)
+        f.close()
+    except IOError:
+        save_to_json_file([], filename)
+else:
+    try:
+        f = open(filename)
+        f = close()
+        my_list()
+    except IOError:
+        save_to_json_file([], filename)
+        my_list()
