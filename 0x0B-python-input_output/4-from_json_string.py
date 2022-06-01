0dd23a5ef20b2311710e04df807d60987bad2c71
METC <metc_ck1@hotmail.com>
2022-06-01 14:51:13 -0700

    4-from json string typo

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,7 @@ Returns an object (Python data structure) as JSON string
 import json
 
 
-def from_json_string(mystr):
+def from_json_string(my_str):
     """
     The function that returns an object
     (Python data structure) as a JSON string
