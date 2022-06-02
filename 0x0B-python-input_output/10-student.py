df19d071e68ce681554351fd1b889c63cf399e01
METC <metc_ck1@hotmail.com>
2022-06-01 17:28:18 -0700

    10-student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class that defines a student
+"""
+
+
+class Student:
+    """
+    student class with first name, last name, age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            attrs_collect = {}
+            for attrib in attrs:
+                for key, value in self.__dict__.items():
+                    if key == attrib:
+                        attrs_collect = value
+            return attrs_collect
+        else:
+        return (self.__dict__)
