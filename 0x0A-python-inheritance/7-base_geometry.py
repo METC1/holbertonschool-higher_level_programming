de60fc0d27898860e5aeec949a898c13e859d2d4
METC <metc_ck1@hotmail.com>
2022-06-01 22:58:13 -0700

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
         Public method validates if number is an integer
         """
 
-        if type(value) is not  int:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
