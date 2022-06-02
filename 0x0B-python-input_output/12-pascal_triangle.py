54b6a84278dd9984bb958c670188e0719bbd4b36
METC <metc_ck1@hotmail.com>
2022-06-01 20:49:40 -0700

    12 pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Calculates pascal triangle of n levels
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of integers representing
+    the Pascal's triangle of n levels
+    """
+
+    pascal_triangle = []
+    if n > 0:
+        for rows in range(0, n):
+            row = []
+            for cell in range(0, rows):
+                if cell == 1 or cell == rows:
+                    row.append(1)
+                else:
+                    prev_row = pascal_triangle[rows - 1]
+                    cell_calc = prev_row[cell - 1] + prev_row[cell]
+                    row.append(cell_calc)
+            pascal_triangle.append(row)
+    return pascal_triangle
