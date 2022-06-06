1c33b1e494c7201bdc3960f9311df1e031a16e79
METC <metc_ck1@hotmail.com>
2022-06-05 22:52:31 -0700

    rectangle v1

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+Class Rectangle, inherits from Base
+"""
+
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Rectangle initialization
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("Width must be an integer")
+        else:
+            if width <= 0:
+                raise ValueError("Width must be > 0")
+            else:
+                self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        else:
+            if height <= 0:
+                raise ValueError("height must be > 0")
+            else:
+                self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        else:
+            if x < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        else:
+            if y < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = y
