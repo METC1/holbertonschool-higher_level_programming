6ebf8891e9a5b2e1cea3fb1eb8508150846fadfb
METC <metc_ck1@hotmail.com>
2022-06-05 18:47:47 -0700

    first upload

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class first create
+"""
+
+
+class Base:
+    """
+    Class Base manages the id attribute in all future classes
+    and avoid duplicating the same code
+    """
+    __nb_objects = 0
+    def __init__ (self, id=None):
+        if id == None:
+            Base.__nb_objects+=1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
