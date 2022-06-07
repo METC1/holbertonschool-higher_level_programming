de44cca1afe3df9ae2fc56fe9fde9bc55b188a56
METC <metc_ck1@hotmail.com>
2022-06-06 22:31:25 -0700

    pycodestyle v2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,36 +17,37 @@ class Square(Rectangle):
         Square initialization
         """
         super().__init__(size, size, x, y, id)
-   
+
     def __str__(self):
-        return '[Square] ({}) {}/{} - {}'.format(self.id, super().x, super().y, super().width)
+        return '[Square] ({}) {}/{} - {}'.format(
+            self.id, super().x, super().y, super().width)
+
     def update(self, *args, **kwargs):
         """
-        Assigns an argument to each attribute of Rectangle
+        Assigns an argument to each attribute of Square
         """
         if len(args) > 0:
             for i, arg in enumerate(args):
                 if i == 0:
-                    self.id =arg
+                    self.id = arg
                 elif i == 1:
                     self.width = arg
-                elif i == 2:
                     self.height = arg
-                elif i == 3:
+                elif i == 2:
                     self.x = arg
-                elif i == 4:
+                elif i == 3:
                     self.y = arg
         else:
             if "id" in kwargs:
                 self.id = kwargs["id"]
-            if "width" in kwargs:
-                self.width = kwargs ["width"]
-            if "height" in kwargs:
-                self.height = kwargs ["height"]
+            if "size" in kwargs:
+                self.width = kwargs["size"]
+                self.height = kwargs["size"]
             if "x" in kwargs:
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
+
     @property
     def size(self):
         return self.width
