c99213e0cdab9d04baffbf10d6a253907d5ccc97
METC <metc_ck1@hotmail.com>
2022-06-10 21:29:48 -0700

    json to file base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,3 +28,15 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return json.dumps([])
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON representation of list_objs to a file
+        """
+        if list_objs is None or list_objs == []:
+            listo = []
+        else:
+            listo = [i.to_dictionary() for i in list_objs]
+        with open(cls.__name__+".json", "w") as f:
+            f.write(cls.to_json_string(listo))
