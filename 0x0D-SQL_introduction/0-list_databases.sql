e838c238527e3b9ce28820bd1c97a1d91e817ae9
METC <metc_ck1@hotmail.com>
2022-07-05 21:24:25 -0700

    fix comments 0 and 1

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,1 +1,3 @@
+--Script that list all databases of MySQL server
+--command SHOW
 SHOW DATABASES
--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,1 +1,3 @@
+--Script that creates the database hbtn_0c_0
+--if the database already exist script shoul not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0
