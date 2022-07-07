e38403bb4827dae11d8c36f13cfdb92bf6d0d1ac
METC <metc_ck1@hotmail.com>
2022-07-06 17:42:12 -0700

    scripts 5 to 8

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
---creates a tablefirst_table in the current database.
---if the table exists, your script should not fail
+-- creates a tablefirst_table in the current database.
+-- if the table exists, your script should not fail
 CREATE TABLE (
 	id INT,
 	name VARCHAR(256));
