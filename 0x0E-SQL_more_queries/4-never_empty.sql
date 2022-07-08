a17e397d438405380ebe41393463c8927feaf4d9
METC <metc_ck1@hotmail.com>
2022-07-07 20:56:39 -0700

    4 again

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- script that creates the table id_not_null
--- id INT with the default value 1 
+-- id INT with the default value 1
 CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1 NOT NULL, name VARCHAR(256)):
