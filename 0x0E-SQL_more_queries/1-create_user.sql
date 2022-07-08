2877b840c2e4a3df3f15851de01615e8fe9bf10f
METC <metc_ck1@hotmail.com>
2022-07-07 20:31:03 -0700

    fixed typo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates the MySQL server user user_0d_1. 
 -- First created then grant privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
