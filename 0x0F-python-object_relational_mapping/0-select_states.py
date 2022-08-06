ac73edc39dd6a1d4fde08634702b6426fda2964e
METC <metc_ck1@hotmail.com>
2022-08-06 10:32:46 -0700

    0-select_states pycode

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,10 @@ if __name__ == "__main__":
     usr = argv[1]
     pwd = argv[2]
     db = argv[3]
-    con = MySQLdb.connect ( host="localhost", user=usr, port=3306, passwd=pwd, db=db)
+    con = MySQLdb.connect(host="localhost",
+                          user=usr,
+                          port=3306,
+                          passwd=pwd, db=db)
     cur = con.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     records = cur.fetchall()
