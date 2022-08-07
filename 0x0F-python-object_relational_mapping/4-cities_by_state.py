c40f39985a957785d0798703bdd1d0bd24f26299
METC <metc_ck1@hotmail.com>
2022-08-06 20:54:35 -0700

    4-cities_by_state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     usr = argv[1]
     pwd = argv[2]
     db = argv[3]
-    N = argv[4]
     con = MySQLdb.connect(host="localhost",
                           user=usr,
                           port=3306,
