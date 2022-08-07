60c2dc255aa70de942ac4963085ecf8149cec004
METC <metc_ck1@hotmail.com>
2022-08-07 16:50:05 -0700

    12-model_state_update_id_2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script to Add State record of database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    usr = argv[1]
+    pwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        usr, pwd, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter_by(id=2).first()
+    states.name = "New Mexico"
+    session.commit()
