2e02cfe586c3d7e603dfab1b7301957f2d160358
METC <metc_ck1@hotmail.com>
2022-08-06 20:34:46 -0700

    3-my_safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script to connect and list all states from the hbtn_0e_0_usa database
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    usr = argv[1]
+    pwd = argv[2]
+    db = argv[3]
+    N = argv[4]
+    con = MySQLdb.connect(host="localhost",
+                          user=usr,
+                          port=3306,
+                          passwd=pwd, db=db)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY states.name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(N))
+    records = cur.fetchall()
+    for row in records:
+        print(row)
+
+    cur.close()
+    con.close()
