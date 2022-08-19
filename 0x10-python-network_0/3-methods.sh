c54ddcda2e1a1e16ab514cdaae03168e60098131
METC <metc_ck1@hotmail.com>
2022-08-18 20:48:48 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes URL, and then displays the methods the server will accept
-curl -sI -X OPTIONS "$1" | grep "allow" | cut -d " " -f 1
+curl -sI -X OPTIONS "$1" | grep "Allow" | cut -d " " -f 1
