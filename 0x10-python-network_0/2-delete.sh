22c00749a7998a514274e04db53c85b97ffafe66
METC <metc_ck1@hotmail.com>
2022-08-18 20:22:35 -0700

    2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Script that takes URL, sends a DELETE request the the URL 
-#and then displays the body of the response
-curl -s -X "DELETE" -L  "$1"
+#Script that takes URL, sends a DELETE request the the URL and then displays the body of the response
+curl -s -X DELETE -L  "$1"
