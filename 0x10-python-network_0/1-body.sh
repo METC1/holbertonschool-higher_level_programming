b11681ffb9a4a4dd0088ab51656954aba72c475a
METC <metc_ck1@hotmail.com>
2022-08-18 21:16:41 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Script that takes URL, sends a GET requesto the the URL 
-#and then displays only  the body of the response
-curl -sL -X GET "$1"
+#Script that takes URL, sends a GET requesto the the URL and then displays only  the body of the response
+curl -s -X GET "$1"
