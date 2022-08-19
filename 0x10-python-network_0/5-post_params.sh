d1d2cb644120489a092ce4d2d7fbd75d0af6aa8c
METC <metc_ck1@hotmail.com>
2022-08-18 21:13:12 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that takes URL, sends a POST request and then displays the body and send some  header variables
-curl -s -X POST "$1" -d "email= test@gmail.com" -d "subject= I will always be here for PLD"
+curl -s -X POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
