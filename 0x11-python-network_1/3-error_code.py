5d050620a83aca07b74b97ad8487b6c1dc02667d
METC <metc_ck1@hotmail.com>
2022-08-20 10:53:22 -0700

    3-error_code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that takes URL and email as arguments, sends a POST
+request to the URL and displays the body of the response in utf-8
+"""
+
+from urllib import (request, parse)
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = request.Request(url, value_enc)
+    try:
+        with request.urlopen(url) as response:
+            resp = response.read()
+            print(resp.decode('utf-8'))
+    except request.HTTPError as error:
+        print("Error code: {}".format(error.code))
+
