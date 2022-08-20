5f31caa651708b9c39dc2c1b3e895ae277dd20c0
METC <metc_ck1@hotmail.com>
2022-08-19 19:49:17 -0700

    2-post_email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that takes URL and email as arguments, sends a POST
+request to the URL and displays the body of the response in utf-8
+"""
+
+from urllib import (request, parse)
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    value_enc = parse.urlencode(value)
+    value_enc = value_enc.encode('ascii')
+    req = request.Request(url, value_enc)
+    with request.urlopen(url) as response:
+        html = response.read()
+        content = html.decode('utf-8')
+        print(content)
