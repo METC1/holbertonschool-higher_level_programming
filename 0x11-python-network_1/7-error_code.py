ba14ba0d4e466ba708e3d25f153fff840e62c8df
METC <metc_ck1@hotmail.com>
2022-08-20 19:36:07 -0700

    7-error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that takes URL , sends a request to the URL
+and displays the error code  of the response if any
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+    if response:
+        print(response.text)
+    else:
+        print("Error code: {}").format(response.status_code))
