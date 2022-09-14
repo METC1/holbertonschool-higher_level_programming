1b48d20384402ec2bdadf892856bb8159a903b17
METC <metc_ck1@hotmail.com>
2022-09-13 20:40:05 -0700

    1-writeme fix style

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const nameFile = process.argv[2];
 const str = process.argv[3];
 
-fs.writeFile(nameFile, str, 'utf8', function(error, data){
+fs.writeFile(nameFile, str, 'utf8', function (error, data) {
   if (error) {
     return console.error(error);
   }
