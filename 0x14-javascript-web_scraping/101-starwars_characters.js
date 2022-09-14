f9a848d051dcd190af8a4c0829757ed3db740537
METC <metc_ck1@hotmail.com>
2022-09-13 21:11:00 -0700

    refactor 101-starwars_characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,28 @@
 #!/usr/bin/node
-// Script that prints all characters of a Star Wars movie
+// prints all characters of a Star Wars movie in order
 
-const axios = require('axios');
-const args = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
-axios.get(url)
-  .then(function (response) {
-    const characters = response.data.characters;
-    for (let index = 0; index < characters.length; index++) {
-      axios.get(characters[index])
-        .then(function (response2) {
-          console.log(response2.data.name);
-        });
+const request = require('request');
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+
+function kindPromise (characters, numChar) {
+  if (numChar === characters.length) {
+    return;
+  }
+  request(characters[numChar], function (errorC, responseC, bodyC) {
+    if (errorC) {
+      console.log('code:', responseC.statusCode);
     }
+    console.log(JSON.parse(bodyC).name);
+    kindPromise(characters, numChar + 1);
   });
+}
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('code:', response.statusCode);
+  } else {
+    const jsonBody = JSON.parse(body);
+    kindPromise(jsonBody.characters, 0);
+  }
+});
