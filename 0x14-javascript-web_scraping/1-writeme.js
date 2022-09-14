31100c8dff4c54eb994443e2c7eac6d71dd1eec9
METC <metc_ck1@hotmail.com>
2022-09-13 20:33:37 -0700

    1-writeme fix 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-/*
-     writes a string to a file
-*/
+//writes a string to a file
+
 const fs = require('fs');
 const nameFile = process.argv[2];
 const str = process.argv[3];
 
-fs.writeFile(nameFile, str, 'utf8', (err) => {
-  if (err) {
-    return console.error(err);
+fs.writeFile(nameFile, str, 'utf8', function(error, data){
+  if (error) {
+    return console.error(error);
   }
 });
